6f6d0909ce42e0d4ef68ca66810a84eee98efa45
DjK <karwowski.d@o2.pl>
2024-02-06 21:02:45 +0100

    Add  application and activity base

---
+++ DjX.Mvvm/Platforms/Android/DjXApplication.cs
@@ -0,0 +1,15 @@
+﻿#if ANDROID21_0_OR_GREATER
+using DjX.Mvvm.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DjX.Mvvm.Platforms.Android;
+public abstract class DjXApplication : Application
+{
+    public abstract T CreateViewModel<T>()
+        where T : ViewModelBase;
+}
+#endif
