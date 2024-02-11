1f9f1d7bd75576c88efc9bdfef11dc005996f1f8
DjK <karwowski.d@o2.pl>
2024-02-11 11:10:11 +0100

    Add binding to base activity

--- DjX.Mvvm/Binding/AndroidBindingObject.cs
+++ DjX.Mvvm/Binding/AndroidBindingObject.cs
@@ -3,20 +3,73 @@ using Android.Text;
 using Android.Views;
 using DjX.Mvvm.Binding.Abstractions;
 using DjX.Mvvm.Commands.Abstractions;
+using DjX.Mvvm.ViewModels;
 using System.ComponentModel;
+using System.Reflection;
 
 namespace DjX.Mvvm.Binding;
+
 public sealed class AndroidBindingObject : IDisposable
 {
-    List<AndroidPropertyBindingSet> PropertyBindings { get; } = [];
-    List<AndroidEventBindingSet> EventBindings { get; } = [];
+    private List<AndroidPropertyBindingSet> PropertyBindings { get; } = [];
+    private List<AndroidEventBindingSet> EventBindings { get; } = [];
+
+    public void RegisterPropertyBindingSet(ViewModelBase sourceObject, View targetObject, string bindingDeclaration) =>
+        RegisterBindingSet(sourceObject, targetObject, bindingDeclaration, RegisterPropertyBindingSet);
+
+    public void RegisterEventBindingSet(ViewModelBase sourceObject, View targetObject, string bindingDeclaration) =>
+        RegisterBindingSet(sourceObject, targetObject, bindingDeclaration, RegisterEventBindingSet);
 
     public void Dispose()
     {
         PropertyBindings.ForEach(pb => pb.Dispose());
         EventBindings.ForEach(eb => eb.Dispose());
     }
+
+    private void RegisterBindingSet(ViewModelBase sourceObject, View targetObject, string bindingDeclaration,
+        Action<ViewModelBase, View, ParsedBinding, PropertyInfo?> registerBindingSet)
+    {
+        var parsedBinding = ParseBindingDeclaration(bindingDeclaration);
+
+        if (parsedBinding is not null)
+        {
+            var sourceProperty = sourceObject.GetType().GetProperty(parsedBinding.SourceMemberName);
+            registerBindingSet(sourceObject, targetObject, parsedBinding, sourceProperty);
+        }
+    }
+
+    private void RegisterPropertyBindingSet(ViewModelBase sourceObject, View targetObject, ParsedBinding parsedBinding, PropertyInfo? sourceProperty)
+    {
+        if (sourceProperty is not null)
+        {
+            var propertyBinding = new AndroidPropertyBindingSet(sourceObject, parsedBinding.SourceMemberName, targetObject, parsedBinding.TargetMemberName);
+            PropertyBindings.Add(propertyBinding);
+        }
+    }
+
+    private void RegisterEventBindingSet(ViewModelBase sourceObject, View targetObject, ParsedBinding parsedBinding, PropertyInfo? sourceProperty)
+    {
+        if (sourceProperty?.PropertyType.GetInterface(nameof(IDjXCommand)) is not null)
+        {
+            var eventBinding = new AndroidEventBindingSet(sourceObject, parsedBinding.SourceMemberName, targetObject, parsedBinding.TargetMemberName);
+            EventBindings.Add(eventBinding);
+        }
+    }
+
+    private static ParsedBinding? ParseBindingDeclaration(string bindingDeclaration)
+    {
+        var bindingParts = bindingDeclaration.Split(' ');
+
+        if (bindingParts.Length != 2)
+        {
+            return null;
+        }
+
+        return new ParsedBinding(bindingParts[1], bindingParts[0]);
+    }
 }
+
+public record ParsedBinding(string SourceMemberName, string TargetMemberName);
 
 public sealed class AndroidPropertyBindingSet : BindingSet<View>
 {
--- DjX.Mvvm/Views/DjXActivityBase.cs
+++ DjX.Mvvm/Views/DjXActivityBase.cs
@@ -1,5 +1,8 @@
 ﻿#if ANDROID21_0_OR_GREATER
-using Android.OS;
+using Android.Content;
+using Android.Util;
+using Android.Views;
+using DjX.Mvvm.Binding;
 using DjX.Mvvm.Platforms.Android;
 using DjX.Mvvm.ViewModels;
 
@@ -8,11 +11,11 @@ namespace DjX.Mvvm.Views;
 public abstract class DjXActivityBase<T> : Activity
     where T : ViewModelBase
 {
+    private readonly AndroidBindingObject bindingObject = new();
     public T ViewModel { get; private set; }
 
-    protected override void OnCreate(Bundle? savedInstanceState)
+    public DjXActivityBase()
     {
-        base.OnCreate(savedInstanceState);
         if (Application is DjXApplication djXApplication)
         {
             ViewModel = djXApplication.CreateViewModel<T>();
@@ -23,10 +26,51 @@ public abstract class DjXActivityBase<T> : Activity
         }
     }
 
+    public override View? OnCreateView(View? parent, string name, Context context, IAttributeSet attrs)
+    {
+        var namespaceUri = "http://schemas.android.com/apk/res-auto";
+        var propertyBindingToParse = attrs.GetAttributeValue(namespaceUri, "bind_property");
+        var eventBindingToParse = attrs.GetAttributeValue(namespaceUri, "bind_event");
+
+        View? view = CreateView(parent, name, context, attrs);
+
+        if (view is not null)
+        {
+            if (propertyBindingToParse is not null)
+            {
+                bindingObject.RegisterPropertyBindingSet(ViewModel, view, propertyBindingToParse);
+            }
+
+            if (eventBindingToParse is not null)
+            {
+                bindingObject.RegisterEventBindingSet(ViewModel, view, eventBindingToParse);
+            }
+        }
+
+        return view;
+    }
+
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+    }
+
     protected override void OnDestroy()
     {
         ViewModel.OnViewModelDestroy();
+        bindingObject.Dispose();
         base.OnDestroy();
+    }
+
+    private View? CreateView(View? parent, string name, Context context, IAttributeSet attrs)
+    {
+        return name switch
+        {
+            "EditText" => new EditText(context, attrs),
+            "TextView" => new TextView(context, attrs),
+            "Button" => new Button(context, attrs),
+            _ => base.OnCreateView(parent, name, context, attrs),
+        };
     }
 }
 #endif
