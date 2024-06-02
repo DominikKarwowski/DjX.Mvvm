839f3e5739abcee089ed33f735999af9c7c4893e
DjK <karwowski.d@o2.pl>
2024-06-02 11:16:55 +0200

    Add navigation implementation scaffolding for Android

---
+++ DjX.Mvvm/Navigation/AndroidNavigationService.cs
@@ -0,0 +1,13 @@
+﻿#if ANDROID21_0_OR_GREATER
+using DjX.Mvvm.Navigation.Abstractions;
+using DjX.Mvvm.ViewModels;
+
+namespace DjX.Mvvm.Navigation;
+public class AndroidNavigationService : INavigationService
+{
+    public void NavigateTo<TViewModel>() where TViewModel : ViewModelBase
+    {
+
+    }
+}
+#endif
