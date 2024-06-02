47cdd4be6fb0b992711f3ebe4b944fe5d69e8f14
DjK <karwowski.d@o2.pl>
2024-06-02 11:41:23 +0200

    Add dummy navigation handling

--- DjX.Mvvm/Navigation/AndroidNavigationService.cs
+++ DjX.Mvvm/Navigation/AndroidNavigationService.cs
@@ -1,13 +1,14 @@
 ﻿#if ANDROID21_0_OR_GREATER
 using DjX.Mvvm.Navigation.Abstractions;
 using DjX.Mvvm.ViewModels;
+using System.Diagnostics;
 
 namespace DjX.Mvvm.Navigation;
 public class AndroidNavigationService : INavigationService
 {
     public void NavigateTo<TViewModel>() where TViewModel : ViewModelBase
     {
-
+        Debug.WriteLine($"Navigating to {typeof(TViewModel).Name}");
     }
 }
 #endif
