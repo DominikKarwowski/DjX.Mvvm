bda651a5f7aaef7c8d912166865ac69a0ae4ec1b
DjK <karwowski.d@o2.pl>
2024-06-29 16:43:31 +0200

    Refactor to use primary ctor

--- DjX.Mvvm/Platforms/Android/DjXApplication.cs
+++ DjX.Mvvm/Platforms/Android/DjXApplication.cs
@@ -4,16 +4,11 @@ using DjX.Mvvm.Navigation.Abstractions;
 using DjX.Mvvm.ViewModels;
 
 namespace DjX.Mvvm.Platforms.Android;
-public abstract class DjXApplication : Application
+public abstract class DjXApplication(IntPtr handle, JniHandleOwnership transfer) : Application(handle, transfer)
 {
     public abstract INavigationService GetNavigationService();
 
     public abstract T CreateViewModel<T>()
         where T : ViewModelBase;
-
-    public DjXApplication(IntPtr handle, JniHandleOwnership transfer)
-        : base(handle, transfer)
-    {
-    }
 }
 #endif
