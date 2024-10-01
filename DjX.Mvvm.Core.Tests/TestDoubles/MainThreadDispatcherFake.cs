686e90299018f3134a913a9d98a4cc1793c7247a
DjK <karwowski.d@o2.pl>
2024-10-01 19:58:17 +0200

    Refactor tests - update test project structure

---
+++ DjX.Mvvm.Core.Tests/TestDoubles/MainThreadDispatcherFake.cs
@@ -0,0 +1,8 @@
+﻿using DjX.Mvvm.Core.Threading.Abstractions;
+
+namespace DjX.Mvvm.Core.Tests.TestDoubles;
+public class MainThreadDispatcherFake : IMainThreadDispatcher
+{
+    public void RunOnMainThread(Action callback) => callback();
+    public Action<T> ScheduleOnMainThread<T>(Action<T> callback) => callback;
+}
