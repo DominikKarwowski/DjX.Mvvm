bd66ee541da333b0deb6ae635ebd0a2a7e82a9c3
DjX <karwowski.d@o2.pl>
2025-07-13 18:04:08 +0200

    Fix tests

--- DjX.Mvvm.Core.Tests/Commands/Helpers/ArgumentValidatorTests.cs
+++ DjX.Mvvm.Core.Tests/Commands/Helpers/ArgumentValidatorTests.cs
@@ -5,7 +5,7 @@ namespace DjX.Mvvm.Core.Tests.Commands.Helpers;
 [TestFixture]
 public class ArgumentValidatorTests
 {
-    public static object[] ValidObjectCases =
+    private static object[] validObjectCases =
     [
         new object[] { 1, typeof(int) },
         new object[] { true, typeof(bool) },
@@ -14,43 +14,46 @@ public class ArgumentValidatorTests
         new object[] { new List<string>{ "abc", "xyz" }, typeof(List<string>) },
     ];
 
-    public static object[] InvalidObjectCases =
-    [
-        new object[] { 1, typeof(decimal) },
-        new object[] { true, typeof(string) },
-        new object[] { "hello", typeof(char) },
-        new object[] { new DateTime(1985, 07, 23), typeof(TimeSpan) },
-        new object[] { new List<string>{ "abc", "xyz" }, typeof(IEnumerable<string>) },
-    ];
-    
-    [TestCaseSource(nameof(ValidObjectCases))]
-    public void ThrowIfNullOrNotOfType_does_not_throw_if_parameter_is_valid(object param, Type expectedType)
+    [TestCaseSource(nameof(validObjectCases))]
+    public void ThrowIfNotOfType_does_not_throw_if_parameter_is_valid(object param, Type expectedType)
     {
         var sut = typeof(ArgumentValidator)
-            .GetMethod(nameof(ArgumentValidator.ThrowIfNullOrNotOfType))!
+            .GetMethod(nameof(ArgumentValidator.ThrowIfNotOfType))!
             .MakeGenericMethod(expectedType);
         
-        Assert.DoesNotThrow(
-            () => sut.Invoke(null, [param]));
+        Assert.DoesNotThrow(() => sut.Invoke(null, [param]));
     }
 
     [Test]
-    public void ThrowIfNullOrNotOfType_throws_if_parameter_is_null()
+    public void ThrowIfNotOfType_does_not_throw_if_parameter_is_null()
     {
-        object? testParam = null;
-        
-        Assert.Throws<ArgumentException>(
-            () => ArgumentValidator.ThrowIfNullOrNotOfType<object?>(testParam),
-            "The parameter cannot be null and must be of type System.Int32");
+        Assert.DoesNotThrow(() => ArgumentValidator.ThrowIfNotOfType<object?>(null));
     }
     
     [Test]
-    public void ThrowIfNullOrNotOfType_throws_if_parameter_is_of_incorrect_type()
+    public void ThrowIfNotOfType_throws_if_parameter_is_of_incorrect_type()
     {
-        object? testParam = null;
-        
-        Assert.Throws<ArgumentException>(
-            () => ArgumentValidator.ThrowIfNullOrNotOfType<int>(testParam),
-            "The parameter cannot be null and must be of type System.Int32");
+        Assert.Multiple(() =>
+        {
+            Assert.Throws<ArgumentException>(
+                () => ArgumentValidator.ThrowIfNotOfType<decimal>(1),
+                $"Non null parameter must be of type {typeof(decimal).FullName}");
+
+            Assert.Throws<ArgumentException>(
+                () => ArgumentValidator.ThrowIfNotOfType<string>(true),
+                $"Non null parameter must be of type {typeof(decimal).FullName}");
+
+            Assert.Throws<ArgumentException>(
+                () => ArgumentValidator.ThrowIfNotOfType<char>("hello"),
+                $"Non null parameter must be of type {typeof(char).FullName}");
+
+            Assert.Throws<ArgumentException>(
+                () => ArgumentValidator.ThrowIfNotOfType<TimeSpan>(new DateTime(1985, 07, 23)),
+                $"Non null parameter must be of type {typeof(TimeSpan).FullName}");
+
+            Assert.Throws<ArgumentException>(
+                () => ArgumentValidator.ThrowIfNotOfType<IEnumerable<string>>(new List<string>{ "abc", "xyz" }),
+                $"Non null parameter must be of type {typeof(IEnumerable<string>).FullName}");
+        });        
     }
 }
--- DjX.Mvvm.Core/Commands/Helpers/ArgumentValidator.cs
+++ DjX.Mvvm.Core/Commands/Helpers/ArgumentValidator.cs
@@ -6,7 +6,7 @@ public static class ArgumentValidator
     {
         if (parameter is not null && parameter.GetType() != typeof(T))
         {
-            throw new ArgumentException("The parameter cannot be null and must be of type " + typeof(T).FullName);
+            throw new ArgumentException("Non null parameter must be of type " + typeof(T).FullName);
         }
     }
 }
